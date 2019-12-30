2619559052787ab55c73f11356999bdcdf3eb831
kkkananer <59269617+kkkananer@users.noreply.github.com>
2019-12-30 12:29:07 +0800

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#define MAX_LEN 100
+
+using namespace std;
+
+int getMaxSum(int* a, int n) {
+	int max = 0;
+	int temp = 0;
+	for (int i = 0; i < n; i++) {
+		if (temp > 0) {
+			temp += a[i];
+		}
+		else {
+			temp = a[i];
+		}
+		if (max < temp) {
+			max = temp;
+		}
+	}
+	return max;
+}
+
+int main() {
+	int a[MAX_LEN];
+	int maxSum;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	maxSum = getMaxSum(a, n);
+	cout << maxSum << endl;
